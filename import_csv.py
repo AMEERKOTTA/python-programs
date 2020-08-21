909f62bc22976fabb4345dd7c150d1329c917d71
AMEER SHAHUL HAMEED KOTTA <53536783+AMEERKOTTA@users.noreply.github.com>
2020-08-21 14:47:51 +0530

    Add files via upload

---
+++ import_csv.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+from tkinter import filedialog
+import pandas as pd
+
+
+root = tk.Tk()
+
+canvas1 = tk.Canvas(root, width = 300, height = 300, bg = "lightsteelblue2", relief = "raised")
+canvas1.pack()
+
+def getCSV():
+    global df
+
+    import_file_path = filedialog.askopenfilename()
+    df = pd.read_csv(import_file_path)
+    print(df)
+
+browseButton_CSV = tk.Button(text = "     IMPORT CSV FILE      ",
+                            command = getCSV, bg = "red", fg = "gray",
+                            font = ("helvetica", 12, "bold"))
+
+canvas1.create_window(150, 150, window = browseButton_CSV)
+
+root.mainloop()
